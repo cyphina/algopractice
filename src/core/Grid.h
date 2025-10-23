39b49be760187f0388df6006bddfc1fc3a61e57f
Nam Ha <Luvkittykittens@gmail.com>
2025-10-22 23:26:33 -0400

    Added a static grid

--- src/core/Grid.h
+++ src/core/Grid.h
@@ -128,4 +128,162 @@ namespace Core
          throw std::out_of_range{std::format("Row ({}) must be less than height ({}).", Col, m_height)};
       }
    }
+
+   /** Static grid has fixed width and height */
+   template <typename T, int WIDTH = 10, int HEIGHT = 10>
+   class StaticGrid
+   {
+    public:
+      // Prevent doing Grid g = 5;
+      explicit StaticGrid();
+      constexpr StaticGrid(const std::array<std::array<T, WIDTH>, HEIGHT>& Init);
+
+      // Rule of 5 since we specified the destructor
+      virtual ~StaticGrid() = default;
+
+      // Still need the default since it doesn't use overloads if T == E
+      StaticGrid(const StaticGrid& Src)            = default;
+      StaticGrid& operator=(const StaticGrid& Rhs) = default;
+
+      template <typename E, int WIDTH2, int HEIGHT2>
+      StaticGrid(const StaticGrid<E, WIDTH2, HEIGHT2>& Src);
+
+      template <typename E, int WIDTH2, int HEIGHT2>
+      StaticGrid& operator=(const StaticGrid<E, WIDTH2, HEIGHT2>& Rhs);
+
+      StaticGrid(StaticGrid&& Src) noexcept            = default;
+      StaticGrid& operator=(StaticGrid&& Rhs) noexcept = default;
+
+      std::optional<T>&       operator[](std::size_t Row, std::size_t Column);
+      const std::optional<T>& operator[](std::size_t Row, std::size_t Column) const;
+
+      std::optional<T>&       At(std::size_t Row, std::size_t Column);
+      const std::optional<T>& At(std::size_t Row, std::size_t Column) const;
+
+      std::size_t GetHeight() const { return m_height; }
+      std::size_t GetWidth() const { return m_width; }
+
+      void Swap(StaticGrid& Other) noexcept;
+
+      static constexpr std::size_t DefaultWidth{10};
+      static constexpr std::size_t DefaultHeight{10};
+
+    private:
+      void VerifyCoordinate(std::size_t Row, std::size_t Col) const;
+
+      std::vector<std::optional<T>> m_cells;
+      std::size_t                   m_width{WIDTH}, m_height{HEIGHT};
+   };
+
+   template <typename T, int WIDTH, int HEIGHT>
+   constexpr StaticGrid<T, WIDTH, HEIGHT>::StaticGrid(const std::array<std::array<T, WIDTH>, HEIGHT>& Init)
+   {
+      size_t NumRows{Init.size()};
+
+      if(NumRows > 0)
+      {
+         size_t NumCols = Init.begin()->size();
+
+         m_cells.resize(NumRows * NumCols);
+
+         for(int i = 0; const auto& Row : Init)
+         {
+            if(Row.size() != NumCols)
+            {
+               throw std::runtime_error("All rows must have the same number of columns!");
+            }
+
+            for(int j = 0; const auto& Column : Row)
+            {
+               (*this)[i, j] = Column;
+               ++j;
+            }
+            ++i;
+         }
+      }
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   std::optional<T>& StaticGrid<T, WIDTH, HEIGHT>::At(std::size_t Row, std::size_t Column)
+   {
+      return const_cast<std::optional<T>&>(std::as_const(*this).At(Row, Column));
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   const std::optional<T>& StaticGrid<T, WIDTH, HEIGHT>::At(std::size_t Row, std::size_t Column) const
+   {
+      VerifyCoordinate(Row, Column);
+      return m_cells[Row * m_width + Column];
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   void StaticGrid<T, WIDTH, HEIGHT>::Swap(StaticGrid& Other) noexcept
+   {
+      std::swap(m_cells, Other.m_cells);
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   const std::optional<T>& StaticGrid<T, WIDTH, HEIGHT>::operator[](std::size_t Row, std::size_t Column) const
+   {
+      return At(Row, Column);
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   StaticGrid<T, WIDTH, HEIGHT>::StaticGrid()
+   {
+      m_cells.resize(WIDTH * HEIGHT);
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   template <typename E, int WIDTH2, int HEIGHT2>
+   StaticGrid<T, WIDTH, HEIGHT>::StaticGrid(const StaticGrid<E, WIDTH2, HEIGHT2>& Src)
+   {
+      m_cells.resize(WIDTH * HEIGHT);
+
+      for(int i = 0; i < HEIGHT; ++i)
+      {
+         for(int j = 0; j < WIDTH; ++j)
+         {
+            if(i < HEIGHT2 && j < WIDTH2)
+            {
+               (*this)[i, j] = Src[i, j];
+            }
+            else
+            {
+               (*this)[i, j].reset();
+            }
+         }
+      }
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   template <typename E, int WIDTH2, int HEIGHT2>
+   StaticGrid<T, WIDTH, HEIGHT>& StaticGrid<T, WIDTH, HEIGHT>::operator=(const StaticGrid<E, WIDTH2, HEIGHT2>& Rhs)
+   {
+      // Note we do overwrite any old values since operator= means we're trying to have something similar to the right not keep anything.
+      StaticGrid<T, WIDTH, HEIGHT> Temp{Rhs}; // Do all work in a temp instance
+      Swap(Temp);                             // Commit work with non-throwing operations.
+      m_cells.resize(WIDTH * HEIGHT);
+      return *this;
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   std::optional<T>& StaticGrid<T, WIDTH, HEIGHT>::operator[](std::size_t Row, std::size_t Column)
+   {
+      return const_cast<std::optional<T>&>(std::as_const(*this)[Row, Column]);
+   }
+
+   template <typename T, int WIDTH, int HEIGHT>
+   void StaticGrid<T, WIDTH, HEIGHT>::VerifyCoordinate(std::size_t Row, std::size_t Col) const
+   {
+      if(Row >= m_height)
+      {
+         throw std::out_of_range{std::format("Row ({}) must be less than width ({}).", Row, m_width)};
+      }
+
+      if(Col >= m_width)
+      {
+         throw std::out_of_range{std::format("Row ({}) must be less than height ({}).", Col, m_height)};
+      }
+   }
 }
--- test/core/TestGrid.cpp
+++ test/core/TestGrid.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <catch2/catch_test_macros.hpp>
 #include "core/Grid.h"
 
@@ -6,4 +7,14 @@ TEST_CASE("Grid At", "[grid]")
    auto Grid{Core::Grid<int>({{1, 2, 3}, {1, 2, 3}})};
    REQUIRE(Grid[0, 2] == 3);
    REQUIRE_THROWS_AS(Core::Grid<int>({{1, 2}, {1, 2, 3}}), std::runtime_error);
+
+   //std::array<std::array<int, 3>, 3> x{{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}}};
+   auto Grid2{Core::StaticGrid<int, 3, 3>({{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}})};
+   auto Grid3{Core::StaticGrid<int, 3, 2>({{{1, 2, 3}, {4, 5, 6}}})};
+
+   Grid2 = Grid3;
+
+   REQUIRE(!Grid2[2, 0].has_value());
+   REQUIRE(!Grid2[2, 1].has_value());
+   REQUIRE(!Grid2[2, 2].has_value());
 }

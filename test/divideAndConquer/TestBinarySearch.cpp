24cb8961a93449dec334f8f4faa8356093a084d9
Zi <cyphina@gmail.com>
2025-12-02 21:56:42 -0500

    Fixed some formatting

--- test/divideAndConquer/TestBinarySearch.cpp
+++ test/divideAndConquer/TestBinarySearch.cpp
@@ -4,31 +4,31 @@
 
 TEST_CASE("Binary Search", "[divideAndConquer]")
 {
-   std::vector<int> empty      = {};
-   std::vector<int> single     = {5};
-   std::vector<int> twoArray   = {4, 6};
-   std::vector<int> threeArray = {4, 10, 18};
+   std::vector<int> Empty      = {};
+   std::vector<int> Single     = {5};
+   std::vector<int> TwoArray   = {4, 6};
+   std::vector<int> ThreeArray = {4, 10, 18};
 
-   auto             view = std::views::iota(1, 100);
-   std::vector<int> hundredArray;
-   std::ranges::copy(view, std::back_inserter(hundredArray));
+   auto             View = std::views::iota(1, 100);
+   std::vector<int> HundredArray;
+   std::ranges::copy(View, std::back_inserter(HundredArray));
 
-   REQUIRE(DivideAndConquer::BinarySearch(empty, 5) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(single, 5) == 0);
-   REQUIRE(DivideAndConquer::BinarySearch(single, 3) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(twoArray, 3) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(twoArray, 4) == 0);
-   REQUIRE(DivideAndConquer::BinarySearch(twoArray, 6) == 1);
-   REQUIRE(DivideAndConquer::BinarySearch(twoArray, 5) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 4) == 0);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 10) == 1);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 18) == 2);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 0) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 5) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 12) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(threeArray, 20) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(hundredArray, 1) == 0);
-   REQUIRE(DivideAndConquer::BinarySearch(hundredArray, 101) == -1);
-   REQUIRE(DivideAndConquer::BinarySearch(hundredArray, 26) == 25);
-   REQUIRE(DivideAndConquer::BinarySearch(hundredArray, 97) == 96);
+   REQUIRE(DivideAndConquer::BinarySearch(Empty, 5) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(Single, 5) == 0);
+   REQUIRE(DivideAndConquer::BinarySearch(Single, 3) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(TwoArray, 3) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(TwoArray, 4) == 0);
+   REQUIRE(DivideAndConquer::BinarySearch(TwoArray, 6) == 1);
+   REQUIRE(DivideAndConquer::BinarySearch(TwoArray, 5) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 4) == 0);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 10) == 1);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 18) == 2);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 0) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 5) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 12) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(ThreeArray, 20) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(HundredArray, 1) == 0);
+   REQUIRE(DivideAndConquer::BinarySearch(HundredArray, 101) == -1);
+   REQUIRE(DivideAndConquer::BinarySearch(HundredArray, 26) == 25);
+   REQUIRE(DivideAndConquer::BinarySearch(HundredArray, 97) == 96);
 }
